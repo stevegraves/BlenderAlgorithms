c8331ebdcd28888d4adeadfe222e3fac8cbe4d14
Steve Graves <steve.graves86@gmail.com>
2021-12-27 21:51:00 -0800

    add loop

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
 import bpy
 
-bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(1, 0, 0), scale=(1, 1, 1))
+num = 20
+offset = 2
+
+while num > 0:
+    bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(offset, 0, 0), scale=(1, offset, offset))
+    num -= 1
+    offset += 2
