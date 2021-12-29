405d9e10aa6a6f0c465b22010571b9019effbbee
Steve Graves <steve.graves86@gmail.com>
2021-12-28 21:51:44 -0800

    Begin keyframing

--- script.py
+++ script.py
@@ -5,5 +5,6 @@ offset = 2
 
 while num > 0:
     bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(offset, 0, 0), scale=(1, offset, offset))
+    bpy.context.object.keyframe_insert(data_path = "scale", frame = offset * 10)
     num -= 1
     offset += 2
