0a92f0816aa29a86f8bacb4418ef51dd26c70b28
Steve Graves <steve.graves86@gmail.com>
2021-12-29 21:19:45 -0800

    Adjust variables

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 import bpy
 
-num = 20
+num = 2
 offset = 2
 
 while num > 0:
     bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(offset, 0, 0), scale=(1, offset, offset))
-    bpy.context.object.keyframe_insert(data_path = "scale", frame = offset * 10)
+    bpy.context.object.keyframe_insert(data_path = "scale", frame = offset * 2)
     num -= 1
     offset += 2
